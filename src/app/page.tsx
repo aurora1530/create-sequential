f90aa1fac28a8b1d42afcda29a56880ec83e9d82
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-03-17 22:05:00 +0900

    feat: メインコンテンツ全体をmainタグで囲い、パディングを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
-import CreateSequential from './components/Sequential';
+import CreateSequential from './components/sequential';
 import Header from './components/Header';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <CreateSequential />
+      <main className="container mx-auto p-4">
+        <CreateSequential />
+      </main>
     </>
   );
 }
