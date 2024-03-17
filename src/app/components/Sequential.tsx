8e3670edd4bda8c172e216af867d5bce112570ee
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-03-17 22:22:58 +0900

    feat: 連番作成の基本機能を作成

--- src/app/components/Sequential.tsx
+++ src/app/components/Sequential.tsx
@@ -1,19 +1,102 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { createSequentialTexts } from '@/app/lib/createSequential';
+import CopyButton from './CopyButton';
 
 export default function CreateSequential() {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [text, setText] = useState<string>('');
+  const [start, setStart] = useState<number>(1);
+  const [stop, setStop] = useState<number>(5);
+  const [step, setStep] = useState<number>(1);
+  const [lineBreak, setLineBreak] = useState<string>('\n');
   const [formatted, setFormatted] = useState<string>('');
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    const serialNumbers = createSequentialTexts(value, { stop: 2 });
-    console.log(serialNumbers);
-    setFormatted(serialNumbers.join('\n'));
+
+  const handleLineBreakChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLineBreak(
+      e.target.value === 'LF' ? '\n' : e.target.value === 'CR' ? '\r' : '\r\n'
+    );
   };
+
+  useEffect(() => {
+    const serialNumbers = createSequentialTexts(text, { start, stop, step });
+    setFormatted(serialNumbers.join('\n'));
+  }, [text, start, stop, step, lineBreak]);
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = 'auto';
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+    }
+  }, [formatted]);
+
   return (
     <>
-      <input type="text" onChange={(e) => onChangeHandler(e)} />
-      <textarea readOnly value={formatted} />
+      <label htmlFor="text" className="block">
+        Text(%dが数字に置き換わります。複数含めることもできます。)
+      </label>
+      <div>
+        <textarea
+          onChange={(e) => setText(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-md"
+        />
+        <label htmlFor="start" className="block mt-4">
+          Start
+        </label>
+        <input
+          type="number"
+          name="start"
+          id="start"
+          value={start}
+          onChange={(e) => setStart(Number(e.target.value))}
+          className="flex-shrink p-2 border border-gray-300 rounded-md"
+        />
+        <label htmlFor="stop" className="block mt-4">
+          Stop
+        </label>
+        <input
+          type="number"
+          name="stop"
+          id="stop"
+          value={stop}
+          onChange={(e) => setStop(Number(e.target.value))}
+          className="p-2 border border-gray-300 rounded-md"
+        />
+        <label htmlFor="step" className="block mt-4">
+          Step
+        </label>
+        <input
+          type="number"
+          name="step"
+          id="step"
+          step={1}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+          className="p-2 border border-gray-300 rounded-md"
+        />
+        <label htmlFor="lineBreak" className="block mt-4">
+          改行コード
+        </label>
+        <select
+          onChange={handleLineBreakChange}
+          className="p-2 border border-gray-300 rounded-md"
+        >
+          <option value="LF">LF</option>
+          <option value="CR">CR</option>
+          <option value="CRLF">CRLF</option>
+        </select>
+      </div>
+      <CopyButton text={formatted} />
+      <textarea
+        ref={textAreaRef}
+        readOnly
+        value={formatted}
+        onChange={(e) => {
+          e.target.style.height = 'auto';
+          e.target.style.height = e.target.scrollHeight + 'px';
+        }}
+        className="mt-4 p-2 border border-gray-300 rounded-md w-full"
+      />
     </>
   );
 }
