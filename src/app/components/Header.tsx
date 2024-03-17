8521d7691f3725d82541742a369596aae9b50432
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-03-17 22:03:46 +0900

    delete: テーマ変更機能は不要なので削除

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,3 @@
-import ThemeSwitcher from './ThemeSwitcher';
-
 export default function Header() {
   return (
     <header className="text-gray-600 body-font bg-blue-500">
@@ -7,7 +5,6 @@ export default function Header() {
         <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
           <span className="ml-3 text-xl">連番作成くん</span>
         </a>
-        <ThemeSwitcher />
         <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             GitHub
