e790649f6538f18ca674c82776f6c35cf795c3e6
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-03-17 22:27:46 +0900

    change: headerの見た目を変更。githubリンクを追加

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,15 @@
 export default function Header() {
   return (
-    <header className="text-gray-600 body-font bg-blue-500">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
-          <span className="ml-3 text-xl">連番作成くん</span>
-        </a>
-        <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            GitHub
-          </button>
-        </div>
-      </div>
-    </header>
+    <div className="bg-blue-500 flex justify-between items-center p-4">
+      <h1 className="text-white font-bold">連番作成くん</h1>
+      <a
+        href="https://github.com/aurora1530/create-sequential"
+        className="bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+    </div>
   );
 }
